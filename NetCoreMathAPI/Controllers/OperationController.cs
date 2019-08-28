0e3c1afd4300555dc05ae0402a4eaf8cde86a5e9
Mert Aksoy <36101620+mertcanaksoy@users.noreply.github.com>
2019-08-28 17:54:20 +0300

    Addition, Subtraction, Exponentiate Operations

---
+++ NetCoreMathAPI/Controllers/OperationController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreMathAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NetCoreMathAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class OperationController : Controller
+    {
+        [Route("/operation/addition/{parameter1}/{parameter2}")]
+        [HttpGet]
+        [HttpPost]
+        public int AdditionResult(int parameter1, int parameter2)
+        {
+            int result = Operation.Addition(parameter1, parameter2);
+            Console.WriteLine("Addition Succeed "+result);
+            return result;
+        }
+
+        [Route("/operation/subtraction/{parameter1}/{parameter2}")]
+        [HttpGet]
+        [HttpPost]
+        public int SubtractionResult(int parameter1, int parameter2)
+        {
+            int result = Operation.Subtraction(parameter1, parameter2);
+            Console.WriteLine("Subtraction Succeed " + result);
+            return result;
+        }
+
+        [Route("/operation/pow/{parameter1}/{parameter2}")]
+        [HttpGet]
+        [HttpPost]
+        public double PowResult(int parameter1, int parameter2)
+        {
+            double result = Operation.Pow(parameter1, parameter2);
+            Console.WriteLine("Pow Succeed " + result);
+            return result;
+        }
+
+        
+
+    }
+}
---
+++ NetCoreMathAPI/Models/Operation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreMathAPI.Models
+{
+    public class Operation
+    {
+        public static int Addition(int param1, int param2)
+        {
+            return param1 + param2;
+        }
+
+        public static int Subtraction(int param1, int param2)
+        {
+            return param1 - param2;
+        }
+
+        public static double Pow(int param1, int param2)
+        {
+            return Math.Pow(param1, param2);
+        }
+    }
+}
